966ada69d2a593c7d9b1426a8d21314e401d05b8
flavia <flaviarcons@yahoo.com.br>
2018-10-03 22:11:52 -0300

    Adicionado index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <Doctype html>
 <html lang=pt-BR>
 	<body>
-	  <?php echo "Ola mundo!".PHP_EOL; ?>
+	  <?php echo "Olá mundo!".PHP_EOL; ?>
+	  <?php echo "Oi".PHP_EOL;?>
 	</body>
 </html>
---
+++ index.php~
@@ -0,0 +1,6 @@
+<Doctype html>
+<html lang=pt-BR>
+	<body>
+	  <?php echo "Ola mundo!".PHP_EOL; ?>
+	</body>
+</html>
